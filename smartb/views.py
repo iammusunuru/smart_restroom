48f52392fd3876507f4f2d1d834a173bbe56850a
iamusunuru <harsha.musunuru@gmail.com>
2016-05-09 18:34:38 -0700

    template support added

--- smartb/views.py
+++ smartb/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from smartb import db_layer
+from smartb.db_layer import db_layer
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -9,9 +10,18 @@ def home(request):
     return render_to_response("home.html")
 
 def loaddata(request):
-    wash_id = request.GET.get("wash_id","")
-    if wash_id:
+    restroom_id = request.GET.get("restroom_id","")
+    if restroom_id:
         db = db_layer("iot_data")
         data = db.get_data({"tag":"real_time_data"})[0]
-        print data
-    return HttpResponse("sucess")
+        data = data["data"]
+        if str(data.get("restroom_id")) == restroom_id:
+            data=json.dumps(data)
+            print data
+            return HttpResponse(data)
+    return HttpResponse("")
+
+
+def load_html(request):
+    page_name = request.GET.get("name","")
+    return render_to_response(page_name+".html")
